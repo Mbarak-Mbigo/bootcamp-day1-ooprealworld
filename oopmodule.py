e0b30dacbde772bbd7fef62594a3ed38c7845040
dreamerTram <mbigombaraktram@gmail.com>
2017-01-17 19:23:31 +0300

    Inheritance implemented

---
+++ oopmodule.py
@@ -0,0 +1,20 @@
+class Person(object):
+	"""docstring for ClassName"""
+	numofpple = 0
+	def __init__(self, name, dob, gender, nationality):
+		self.name = name
+		self.dob = dob
+		self.gender = gender
+		self.nationality = nationality
+		
+class Student(Person):
+	pass
+
+class Staff(Person):
+	pass
+
+
+		
+
+		
+		
